9da210c3fe3da69e411cafd1d64882754681976b
PVSKR <skr.polu@hotmail.com>
2023-06-07 23:11:51 +0530

    main-grid aside  the page in stripedemopage

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div class="main-grid">
+   <main class="testimonial-grid">
+  
+ 
+   <div class="flex">
+     
+     <img class="border-primary-400" src="./images/madhu.jpg" alt="Patrick Abrams" />
+
+  
+ </div>
+ <div class="flex">
+ 
+     <img class="border-primary-400" src="./images/madhu.jpg" alt="Patrick Abrams" />
+   
+ 
+ </div>
+ <div class="flex">
+   
+     <img class="border-primary-400" src="./images/madhu.jpg" alt="Patrick Abrams" />
+   
+ 
+ </div>
+   
+   
+   </main>
+    <aside>
+    <p className='eyebrow'>Skills & Experience</p>
+   <h1 className='page-title'>MY Resume</h1>
+    </aside>
+ </div>
+  );
+}
+
+export default App;
