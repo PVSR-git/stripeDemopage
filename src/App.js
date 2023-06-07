dfb3360da05fc721258f60010bc2d04546a303ca
PVSKR <skr.polu@hotmail.com>
2023-06-08 00:02:04 +0530

    stripedemopage step1of development completed.

--- src/App.js
+++ src/App.js
@@ -4,33 +4,56 @@ import './App.css';
 function App() {
   return (
     <div class="main-grid">
-   <main class="testimonial-grid">
-  
+     <main class="testimonial-grid">
+    <article class="testimonial grid-col-span-2 flow bg-primary-400 quote text-neutral-100">
+      <div class="flex">
+        <div>
+          <img  src="./images/madhu.jpg" alt="daniel clifford"/>
+        </div>
+      
+      </div>
  
-   <div class="flex">
+    </article>
+    <article class="testimonial flow bg-secondary-400 text-neutral-100">
+      <div class="flex">
+        <div>
+          <img src="./images/madhu.jpg" alt="Jonathan Walters"/>
+        </div>
+       
+       
+      </div>
+    
+    </article>
+    <article class="testimonial flow bg-neutral-100 text-secondary-400">
+      <div class="flex">
+        <div>
+          <img src="./images/madhu.jpg" alt="Jeanette Harmon" />
+        </div>
+       
+      </div>
      
-     <img class="border-primary-400" src="./images/madhu.jpg" alt="Patrick Abrams" />
-
-  
- </div>
- <div class="flex">
+    </article>
+    <article class="testimonial grid-col-span-2 flow bg-secondary-500 text-neutral-100">
+      <div class="flex">
+        <div>
+          <img class="border-primary-400" src="./images/madhu.jpg" alt="Patrick Abrams" />
+        </div>
+      
+      </div>
  
-     <img class="border-primary-400" src="./images/madhu.jpg" alt="Patrick Abrams" />
-   
- 
- </div>
- <div class="flex">
-   
-     <img class="border-primary-400" src="./images/madhu.jpg" alt="Patrick Abrams" />
-   
- 
- </div>
-   
-   
-   </main>
+    </article>
+    <article class="testimonial flow bg-neutral-100 text-secondary-400">
+      <div class="flex">
+        <div>
+          <img src="./images/madhu.jpg" alt="Kira Whittle" />
+        </div>
+       </div>
+    </article>
+  </main>
     <aside>
-    <p className='eyebrow'>Skills & Experience</p>
-   <h1 className='page-title'>MY Resume</h1>
+    
+   <h1 className='page-title'>Seeking: impatient,curious optimists</h1>
+   <p className='eyebrow'>Stripe is for people who not only enjoy solving hard problems, they thrive on it. if that sounds like you ,come and play a lead role in the next chapter of out story.</p>
     </aside>
  </div>
   );
