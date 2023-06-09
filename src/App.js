4368257267ca603d641bc8b15a1122312ba32f4d
PVSKR <skr.polu@hotmail.com>
2023-06-09 13:13:42 +0530

    stripedDemo css  final touch

--- src/App.js
+++ src/App.js
@@ -5,50 +5,17 @@ function App() {
   return (
     <div class="main-grid">
      <main class="testimonial-grid">
-    <article class="testimonial grid-col-span-2 flow bg-primary-400 quote text-neutral-100">
-      <div class="flex">
-        <div>
-          <img  src="./images/madhu.jpg" alt="daniel clifford"/>
-        </div>
-      
-      </div>
- 
-    </article>
-    <article class="testimonial flow bg-secondary-400 text-neutral-100">
-      <div class="flex">
-        <div>
-          <img src="./images/madhu.jpg" alt="Jonathan Walters"/>
-        </div>
-       
-       
-      </div>
-    
-    </article>
-    <article class="testimonial flow bg-neutral-100 text-secondary-400">
-      <div class="flex">
-        <div>
-          <img src="./images/madhu.jpg" alt="Jeanette Harmon" />
-        </div>
-       
-      </div>
-     
-    </article>
-    <article class="testimonial grid-col-span-2 flow bg-secondary-500 text-neutral-100">
-      <div class="flex">
-        <div>
-          <img class="border-primary-400" src="./images/madhu.jpg" alt="Patrick Abrams" />
-        </div>
-      
-      </div>
- 
-    </article>
-    <article class="testimonial flow bg-neutral-100 text-secondary-400">
-      <div class="flex">
-        <div>
-          <img src="./images/madhu.jpg" alt="Kira Whittle" />
-        </div>
-       </div>
-    </article>
+     <div class="flex">
+     <img  src="./images/madhu.jpg"  class="testimonial grid-col-span-1" alt="daniel clifford"/></div>
+     <div class="flex">
+     <img src="./images/madhu.jpg" class="testimonial grid-col-span-2"/></div>
+     <div class="flex">
+     <img src="./images/madhu.jpg" class="testimonial grid-col-span-1" alt="Jeanette Harmon" /></div>
+     <div class="flex">
+     <img src="./images/madhu.jpg" class="testimonial grid-col-span-2" alt="Kira Whittle" /></div>
+     <div class="flex">
+     <img  src="./images/madhu.jpg" class="testimonial grid-col-span-1" alt="Patrick Abrams" /></div>
+  
   </main>
     <aside>
     
