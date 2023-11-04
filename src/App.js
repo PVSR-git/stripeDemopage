114df51e804ed0777ccc5b62c3fd90e8268827a3
PVSKR <skr.polu@hotmail.com>
2023-11-04 20:50:08 +0530

    modified to support mobilelayout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div class="main-grid">
-     <main class="testimonial-grid">
-     <div class="flex">
-     <img  src="./images/1.png" style={{  width: '304px' ,height:'210px'}} class="testimonial" alt="daniel clifford"/></div>
-     <div class="flex ">
-     <img src="./images/carpenter.jpg" style={{  width: '230px' ,height:'300px'}} /></div>
-     <div class="flex ">
-     <img src="./images/2.png" style={{  width: '304px' ,height:'210px'}}   class="testimonial" alt="Jeanette Harmon" /></div>
-     <div class="flex1">
-     <img src="./images/4.png" style={{  width: '230px' ,height:'300px'}} class="testimonial " alt="Kira Whittle" /></div>
-     <div class="flex1 " >
-     <img  src="./images/3.png" style={{  width: '304px' ,height:'200px'}}  class="testimonial" alt="Patrick Abrams" /></div>
-  
-  </main>
-    <aside>
-    
-   <h1 className='page-title'>Seeking: impatient,curious optimists</h1>
-   <p className='eyebrow'>Stripe is for people who not only enjoy solving hard problems, they thrive on it. if that sounds like you ,come and play a lead role in the next chapter of out story.</p>
-    </aside>
- </div>
+      <main class="testimonial-grid">
+        <div class="flex">
+          <img
+            src="./images/1.png"
+            style={{ width: "304px", height: "210px" }}
+            class="testimonial"
+            alt="daniel clifford"
+          />
+        </div>
+        <div class="flex ">
+          <img
+            src="./images/carpenter.jpg"
+            style={{ width: "230px", height: "300px" }}
+            class="testimonial"
+          />
+        </div>
+        <div class="flex ">
+          <img
+            src="./images/2.png"
+            style={{ width: "304px", height: "210px" }}
+            class="testimonial"
+            alt="Jeanette Harmon"
+          />
+        </div>
+        <div class="flex1">
+          <img
+            src="./images/4.png"
+            style={{ width: "230px", height: "300px" }}
+            class="testimonial "
+            alt="Kira Whittle"
+          />
+        </div>
+        <div class="flex1 ">
+          <img
+            src="./images/3.png"
+            style={{ width: "304px", height: "200px" }}
+            class="testimonial"
+            alt="Patrick Abrams"
+          />
+        </div>
+      </main>
+      <aside>
+        <h1 className="page-title">Seeking: impatient,curious optimists</h1>
+        <p className="eyebrow">
+          Stripe is for people who not only enjoy solving hard problems, they
+          thrive on it. if that sounds like you ,come and play a lead role in
+          the next chapter of out story.
+        </p>
+      </aside>
+    </div>
   );
 }
 
